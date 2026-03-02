febc20a6e7f497d215c6a48fa4e03c7787c0f8da
wnsgud4300 <wnsgud4300@gmail.com>
2026-03-02 05:17:02 +0000

    feat: revamp airplane race UI with vertical sky track and live formula input

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/orbitron/700.css'
+import '@fontsource/orbitron/900.css'
+import '@fontsource/rajdhani/400.css'
+import '@fontsource/rajdhani/600.css'
+import '@fontsource/rajdhani/700.css'
 import './index.css'
 import App from './App.tsx'
 
